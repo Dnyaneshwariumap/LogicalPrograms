003d2530697ad36f50f2f78cd800fab01f13999c
Dnyaneshwari <dnyaneshwariumap994@gmail.com>
2022-09-24 17:11:17 +0530

    added:Logical Programs

---
+++ FibonacciSeries.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.day6;
+import java.util.Scanner;
+public class FibonacciSeries {
+    public static void main(String[] args) {
+
+        int n1=0, n2=1, n3;
+        try (Scanner sc  = new Scanner(System.in)) {
+            System.out.println("enter the number");
+            int number = sc.nextInt();
+            for(int i=1; i<=number; i++) {
+                System.out.print(n1+" ");
+                n3 = n1+ n2;
+                n1=n2;
+                n2=n3;
+            }
+        }
+
+    }
+}
